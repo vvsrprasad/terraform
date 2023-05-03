08a05239032bde87ff2e3d921846efd7243cfb9d
vvsrprasad <104518755+vvsrprasad@users.noreply.github.com>
2023-05-04 01:08:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 access_key = "AKIASDLFJNTXAZWB6LUC"
 secret_key = "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"
 }
-resource "aws_s3_bucket" "myexample" {
+resource "aws_s3_bucket" "practice" {
 bucket = "rjbucket1"
 tags   = {
 Name = "dev"
