30954c5ab7d211679fc735dcf2de8da8debb7213
vvsrprasad <104518755+vvsrprasad@users.noreply.github.com>
2023-05-04 01:38:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ access_key = "AKIASDLFJNTXJRS2O55X"
 secret_key = "8GtpLDtpOCO9Pe48OICEKa4lEd64QtrCd86ArbwE"
 }
 resource "aws_s3_bucket" "example" {
-bucket = "rj-bucket440"
+bucket = "rj-bucket1"
 tags   = {
 Name = "dev"
 }
