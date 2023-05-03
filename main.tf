0027e11f11cc83d718d964aee7279a0e8a4fcace
Venna12 <52368332+Venna12@users.noreply.github.com>
2023-05-03 12:55:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,9 @@ region = "us-east-1"
 access_key = "AKIAUOPOPAVLMTI6XQYJ"
 secret_key = "T4VHUvsRwmicDtJcSvVEsTUCjKhd7x31fEJaCs36"
 }
-
-resource "aws_s3_bucket" "vcube" {
-bucket = "vcube90143"
+resource "aws_s3_bucket" "example" {
+bucket = "vcube901439878"
 tags   = {
 Name = "dev"
 }
 }
-
